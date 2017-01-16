062ce910d0160d36e49fd3a582ff143bca3308f1
dominic-taylor <dataylorx@gmail.com>
2017-01-16 13:16:21 +1300

    Add box shadow on project card hover and moved listener to project card

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-var projectDetails = document.getElementsByClassName('more')
+var projectCards = document.getElementsByClassName('projectcard')
 
-for (var i = 0; i < projectDetails.length; i++) {
-  projectDetails[i].addEventListener('click', openDescription, false)
+for (var i = 0; i < projectCards.length; i++) {
+  projectCards[i].addEventListener('click', openDescription, false)
 
 }
 
 function openDescription(e) {
-  var projectDesc = e.target.nextElementSibling
+  var listening = this
+  var clicked = e.target.className
+  var projectDesc = this.querySelector('.projectdesc')
+  console.log(listening + ' AND '+projectDesc);
 
   if(projectDesc.classList.contains('closed')){
     projectDesc.classList.add('expanded')
     projectDesc.classList.remove('closed')
-    e.target.innerHTML = 'Less'
+    listening.querySelector('.more').innerHTML = 'Less'
   }
   else {
     projectDesc.classList.add('closed')
     projectDesc.classList.remove('expanded')
-    e.target.innerHTML = 'More...'
+    listening.querySelector('.more').innerHTML = 'More..'
   }
 }
