92626ecbb5cec8085cb5b036360c2fdba66ed3e9
dominic-taylor <dataylorx@gmail.com>
2017-04-03 14:19:42 +1200

    Nav bar and links styled

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 
 var nav = document.getElementsByClassName('nav')
-var icon = document.getElementById('icon')
-icon.addEventListener('click', toggleNav, false)
+var menu = document.getElementById('menu')
+var navLinks = document.getElementsByClassName('navlinks')
+
+console.log(menu)
+menu.addEventListener('click', toggleNav, false)
 
 function toggleNav(e) {
-  if(nav[0].classList.contains('navdown')){
-    nav[0].classList.remove('navdown')
-    icon.innerHTML = '&#9776'
-  } else {
-    nav[0].classList.add('navdown')
-    icon.innerHTML = '&times'
+  console.log(navLinks)
 
+ if(nav[0].classList.contains('navdown')){
+    nav[0].classList.remove('navdown')
+    menu.innerHTML = '&#9776'
+    for (var i = 0; i<navLinks.length; i++) {
+      navLinks[i].classList.remove('visible')
+    }
+  }else {
+    nav[0].classList.add('navdown')
+    menu.innerHTML = '&times'
+    for (var i = 0; i<navLinks.length; i++) {
+      navLinks[i].classList.add('visible')
+    }
   }
 }
 
-
+// Have proejct cards always be open. 
+// Have essentially this add/remove display none on menu items so nav bar is empty (apart from menu) when not down.
 var projectCards = document.getElementsByClassName('projectcard')
 
 for (var i = 0; i < projectCards.length; i++) {
